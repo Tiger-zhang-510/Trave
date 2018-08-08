8e5625d00df49a74451ac9233777cec2bc53da25
zhanghu <18292851838@163.com>
2018-08-08 20:32:53 +0800

    banner-detail

--- src/common/gallary/Gallary.vue
+++ src/common/gallary/Gallary.vue
@@ -1,7 +1,7 @@
 <template>
  <div class="container" @click="handleGallaryClick">
      <div class="wrapper">
-         <swiper :optopns="swiperOptions">
+         <swiper :options="swiperOptions">
              <swiper-slide v-for="(item,index) in imgs" :key="index">
                  <img class="gallary-img" :src="item" alt="">
              </swiper-slide>
@@ -44,24 +44,26 @@
 </script>
 
 <style lang="stylus" scoped>
-.container 
-  display: flex 
-  flex-direction: column 
-  justify-content: center 
-  z-index: 99 
-  position: fixed
-  left: 0 
-  right: 0 
-  top: 0 
-  bottom: 0 
-  background: #000 
-  .wrapper 
-    overflow: hidden 
-    height: 0 
-    width: 100% 
-    padding-bottom: 100% 
-    .gallary-img 
-      width: 100% 
-    .swiper-pagination 
-      color: #fff  
+  .container >>> .swiper-container
+    overflow: inherit
+  .container
+    display: flex
+    flex-direction: column
+    justify-content: center
+    z-index: 99
+    position: fixed
+    left: 0
+    right: 0
+    top: 0
+    bottom: 0
+    background: #000
+    .wrapper
+      height: 0
+      width: 100%
+      padding-bottom: 100%
+      .gallary-img
+        width: 100%
+      .swiper-pagination
+        color: #fff
+        bottom: -1rem
 </style>
